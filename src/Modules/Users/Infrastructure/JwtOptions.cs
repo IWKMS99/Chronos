ca1ac2f2856ba1160d17eb0254096b51a8b6378e
IWKMS99 <alievsaid0909@yandex.ru>
2025-08-22 16:00:44 +0300

    feat(users): добавлена начальная миграция EF и конфигурация JWT

---
+++ src/Modules/Users/Infrastructure/JwtOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Chronos.Users.Infrastructure;
+
+public class JwtOptions {
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+}
---
+++ src/Modules/Users/Migrations/20250822103523_Users_Initial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chronos.Users.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users_Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "users");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                schema: "users",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "users");
+        }
+    }
+}
