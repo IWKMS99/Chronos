042ca732b5c2dde11ccdffe01c054e73501f9e6e
IWKMS99 <alievsaid0909@yandex.ru>
2025-08-22 13:16:21 +0300

    feat(users): реализован эндпоинт для регистрации нового пользователя

---
+++ src/Api/Endpoints/Users/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Chronos.Modules.Users.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chronos.Api.Endpoints.Users;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController : ControllerBase {
+    private readonly ISender _sender;
+
+    public UsersController(ISender sender) {
+        _sender = sender;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request) {
+        var command = new RegisterUserCommand(request.Email, request.Password);
+        await _sender.Send(command);
+        return StatusCode(201);
+    }
+}
